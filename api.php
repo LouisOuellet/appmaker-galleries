07b3b5007d81fa36e0a3f162ca32c657ed2a8a5a
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-04 14:30:19 -0500

    22.01-dev-22

--- api.php
+++ api.php
@@ -1,2 +1,13 @@
 <?php
-class galleriesAPI extends CRUDAPI {}
+class galleriesAPI extends CRUDAPI {
+
+	public function buildRelation($relations,$relation){
+    $recordDetails = $this->Auth->query('SELECT * FROM `pictures` WHERE `dirname` = ?',$relations[$relation['relationship']][$relation['link_to']]['dirname']);
+    if($recordDetails->numRows() > 0){
+      foreach($recordDetails->fetchAll()->All() as $recordDetail){
+        $relations[$relation['relationship']][$relation['link_to']]['pictures'][$recordDetail['id']] = $recordDetail;
+      }
+    } else { $relations[$relation['relationship']][$relation['link_to']]['pictures'] = []; }
+    return $relations;
+  }
+}

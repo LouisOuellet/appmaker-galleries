d7049b9dd8328658162e795f1a6aa001292e460c
Louis Ouellet <louis@TheMacSwitch.local>
2022-01-04 15:41:08 -0500

    22.01-dev-24

---
+++ helper.php
@@ -0,0 +1,13 @@
+<?php
+class galleriesHelper extends Helper {
+
+	public function buildRelation($relations,$relation){
+    $recordDetails = $this->Auth->query('SELECT * FROM `pictures` WHERE `dirname` = ?',$relations[$relation['relationship']][$relation['link_to']]['dirname']);
+    if($recordDetails->numRows() > 0){
+      foreach($recordDetails->fetchAll()->All() as $recordDetail){
+        $relations[$relation['relationship']][$relation['link_to']]['pictures'][$recordDetail['id']] = $recordDetail;
+      }
+    } else { $relations[$relation['relationship']][$relation['link_to']]['pictures'] = []; }
+    return $relations;
+  }
+}
